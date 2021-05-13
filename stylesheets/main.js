6cad395a4c1736c6604fa081c055ec537e500879
leviettien2000 <leviettien2000@gmail.com>
2021-05-13 11:14:24 +0700

    13/5

--- stylesheets/main.js
+++ stylesheets/main.js
@@ -2,32 +2,14 @@ let socket
 let onlineUser=[]
 let username
   window.onload=()=>{
-    console.log("server  đã sẵn sàng hoạt dộng")
-      socket= io()
+    socket= io()
     socket.on('connect',()=>{
       console.log("đã kết nối socket thành công id= ",socket.id)
-      setTimeout(()=>{
-        username = sessionStorage.getItem("username")
-        if(username){
-          console.log(" đã lấy tên người dùng từ session storgare");
-        }
-        if(!username){
-        username= prompt(" nhập tên của bạn")
-        sessionStorage.setItem("username", username)
-      }
-      // gửi tên đăng kí cho server
-      socket.emit('regiter-nameUser', username)
-      },500)// đợi ẩn thông báo xanh và đổ ở dưới góc màn hìnhrồi cho nhập username
-      
     })
     $("#online-notification").fadeTo(10,0)
     $("#offline-notification").fadeTo(10,0)
     socket.on('disconnect',()=>console.log("đã mất kết nối đến server"))
     socket.on('error',()=>console.log("đã xảy ra lỗi: ",e.message))
-    /*socket.on('message',(mess)=>{
-      console.log("tin nhận được: ",mess)
-      socket.send('tin nhăn trả lại từ client')
-    })*/
     //Xu ly thong bao moi
     $('#btnUpload').click(e=>{
       var Title = $('#title').val()
@@ -53,6 +35,7 @@ let username
               txtContent=''
               socket.emit('notify',json.data)
               $('#confirm-post-dialog').modal('hide')
+              window.reload()
             }
               else{
               $('#error').removeAttr('style')
@@ -63,46 +46,11 @@ let username
           .catch(e=>console.log(e))
       }
   })
-      // danh sách người đã vào web nhận từ server 
-      socket.on("list-users",(m)=>{
-        console.log(" đã nhận danh sách user online từ server",m)
-        m.forEach(u=>{
-          console.log(u)
-          if(u.id!==socket.id){
-            onlineUser.push(u)// thêm user vào danh sách người trừ chính mình ( socket là máy của mình )// hiển thị ra danh sách những người đã vào trang web trước đó ( nếu ko có thì người vào sau sẽ ko thấy người vào trước)
-          }
-        })
-      })
-
-      // sựu kiện đăng kí thêm một người dùng mới vào trang web 
-      socket.on("new-user",m=>{
-        console.log("new user connect", m )
-        onlineUser.push(m)
-      })
-      
-      // sự kiệ khi người dùng thoát ra web 
-      socket.on('user-leave',id=>{
-        let user = onlineUser.find(u=>u.id===id)
-        onlineUser = onlineUser.filter(u=>u.id!=id)
-        console.log(`user id ${id} đã thoát, chỉ còn ${onlineUser.length} user trong phòng`)
-        remove(id)
-        notifyOffline(user.username)
-      })
+      // THONG BAO 
       socket.on('alertNoti',s=>{
         let m = s.message
         console.log(m)
         notifyOnline(m)
-      })
-      // nhận thông tin user name từ server
-      socket.on('register-name',data=>{
-        let {id, username}= data
-        let user= onlineUser.find(u=>u.id==id)
-        if(!user){
-          return console.log("khong tim thay user ")
-        }
-        user.username = username
-        console.log(`client ${id} đã đăng kí với tên ${username}`)
-        notifyOnline(username)
       })
     }
 var c = 0
