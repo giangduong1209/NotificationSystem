8e3d59e61c9e2bd4ab6e22ff64d0daed8cdd582f
giangduong1209 <giangduong120900@gmail.com>
2021-05-13 13:37:22 +0700

    Change File

--- routers/student.js
+++ routers/student.js
@@ -3,21 +3,26 @@ const Router = express.Router()
 const parser = require('parser')
 const AccountStudent = require('../models/AccountStudentModel')
 const ObjectID = require('mongodb').ObjectID;
+const app = express()
+app.set('view engine','ejs')
 Router.post('/update',(req,res)=>{
     let result=''
     req.on('data',d=>result+=d.toString())
+
     req.on('end',()=>{
         data = JSON.parse(result)
-        // let stu = new AccountStudentModel({
-        //     title:data,
-        //     context:data.context,
-        //     permission:data.permission
-        // })
-        // noti.save()
-        res.json({code:0,message:'Khong loi',data:noti._id})
+        AccountStudent.findOneAndUpdate(data['emailStu'], data, {
+            new: true
+        })
+
+        .then(p =>{
+            if(p){
+                return console.log({data: p})
+            }
+            else return console.log("Error")
+        })
     })
-    console.log('da nhan anh')
-    
+    return res.render('studentInterface',{data})
 })
 
 module.exports = Router
