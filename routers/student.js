77c8af20d3743be27bfbb72e644be6c47131b119
giangduong1209 <giangduong120900@gmail.com>
2021-05-15 16:45:37 +0700

    Edit and delete

--- routers/student.js
+++ routers/student.js
@@ -50,7 +50,7 @@ Router.post('/upload', (req,res) =>{
             email: dataPost.email,
             titlePost: dataPost.titlePost,
             datePost:dataPost.datePost,
-            contextPost: htmlToText(dataPost.contextPost),
+            contextPost: encodeText,
 
         })
         contPost.save()
@@ -58,6 +58,53 @@ Router.post('/upload', (req,res) =>{
     })
     console.log('Da Nhan')
 })
+
+Router.post('/post',(req,res) =>{
+    let {id} = req.body
+    ContentPost.findOne({_id:ObjectID(id)})
+    .then(p=>{
+        return res.json({code:0,message:'Lấy thành công',data:p})
+    })
+})
+
+Router.post('/post/edit', (req, res) =>{
+    let result=''
+    req.on('data',d=>{
+        result+=d.toString()
+    })
+    req.on('end',()=>{
+        let data = JSON.parse(result)
+        ContentPost.findByIdAndUpdate({_id:ObjectID(data.id)},{titlePost:data.titlePost, contextPost:htmlToText(data.contextPost)})
+        .then(p=>{
+            if(p){
+                return res.json({code:1,message:'Sửa thành công'})
+            }
+            return  res.json({code:0,message:'Sửa thất bại'})
+        })
+        
+    })
+    req.on('error',(e)=>{
+        console.log(e)
+    })
+})
+
+Router.post('/post/delete/:id', (req, res) =>{
+    if(!req.params.id){
+        return res.json({code:1,message:'Invalid ID'})
+    }else{
+        console.log(req.params.id)
+        console.log('Đang xoá')
+        ContentPost.findByIdAndDelete({_id:ObjectID(req.params.id)})
+        .then(p=>{
+            if(p){
+                return res.json({code:0,message:'Xoá thành công'})
+            }else{
+                return res.json({code:1,message:'Xoá thất bại'})
+            }
+        })
+    }
+})
+
 
 Router.get('/', (req, res) =>{
     let emailStu = req.session.user
@@ -67,6 +114,7 @@ Router.get('/', (req, res) =>{
         var emailSt = p.email;
         ContentPost.find({email: emailStu})
         .then(p =>{
+            console.log(p)
             p = p.reverse()
             Notification.find({})
             .then(n=>{
